1956cdca22f47e995c3705695e695ab453ce4d37
ssyssoev <ssyssoev@mail.ru>
2023-07-10 18:15:01 +0300

    Коммит 12 ДЗ (начальный)

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        Author nosov = new Author("Nikolay", "Nosov");
+        Author lermontov = new Author ("Michail", "Lermontov");
+        System.out.println("Nosov.name = " + nosov.getAuthorName()+ " "+ nosov.getAuthorSurname());
+        Book Neznaika = new Book("Neznaika", nosov, 1972);
+        Book Borodino =new Book ("Borodino", lermontov,1812);
+        System.out.println("Nosov.name = " + nosov.getAuthorName()+ " "  + nosov.getAuthorSurname()+ " " + Neznaika.getBookName()+ "  " + Neznaika.getBookYear());
+        System.out.println("Lermontov.name = " + lermontov.getAuthorName()+ " "  + lermontov.getAuthorSurname()+ " " + Borodino.getBookName()+ "  " + Borodino.getBookYear());
+Borodino.setBookYear(2020);
+System.out.println(Borodino.getBookName() + "  было переиздано в  " +Borodino.getBookYear() +" году");
+    }
+}
